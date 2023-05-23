0fa3ac55f9726d80e662d422f044fdfa0e8e1096
goncaloarsenio <goncalo.arsenio92@gmail.com>
2023-05-23 18:11:48 +0100

    login

---
+++ spotify-clone/src/spotify.js
@@ -0,0 +1,19 @@
+//https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
+
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+const redirectUri = "http://localhost:3000/";
+
+const clientId = "32b889f9eea84dadad01419f90974d90";
+
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
