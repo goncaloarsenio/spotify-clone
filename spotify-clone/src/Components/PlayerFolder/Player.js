d44fe5e32a44cb36ec7cb3dca3b3f4b47bc72cc9
goncaloarsenio <goncalo.arsenio92@gmail.com>
2023-05-25 15:54:04 +0100

    sidebar options add

--- spotify-clone/src/Components/PlayerFolder/Player.js
+++ spotify-clone/src/Components/PlayerFolder/Player.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "./Player.css";
 import Sidebar from "../SidebarFolder/Sidebar"
-import Body from '../BodyFolder/Body'
+import Body from "../BodyFolder/Body"
+import Footer from "../FooterFolder/Footer"
+
+
+
 function Player(spotify) {
   return (
     <div className="player">
@@ -9,6 +13,8 @@ function Player(spotify) {
       <Sidebar/>
       <Body />
      </div>
+<Footer />
+
     </div>
   );
 }
