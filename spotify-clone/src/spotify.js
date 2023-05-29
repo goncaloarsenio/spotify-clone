5432d5a44ad4a5d236ae5f3ce649fe13d519482b
goncaloarsenio <goncalo.arsenio92@gmail.com>
2023-05-29 16:26:41 +0100

    deploy test

--- spotify-clone/src/spotify.js
+++ spotify-clone/src/spotify.js
@@ -2,7 +2,7 @@
 
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
-const redirectUri = "http://localhost:3000/";
+const redirectUri = "https://spotify-clone-1c768.web.app/";
 
 const clientId = "32b889f9eea84dadad01419f90974d90";
 
